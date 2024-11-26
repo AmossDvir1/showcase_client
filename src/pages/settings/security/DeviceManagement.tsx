4e45fff38778cf8d4d2318eb88229f6a4b3a180b
AmossDvir1 <dvir.amoss@gmail.com>
2024-11-26 23:18:25 +0200

    DeviceManagement responsiveness

--- src/pages/settings/security/DeviceManagement.tsx
+++ src/pages/settings/security/DeviceManagement.tsx
@@ -88,19 +88,22 @@ const DeviceManagement: React.FC = () => {
         ) : (
           <div className="space-y-4">
             {sessions?.map((session) => (
-                <div
+              <div
                 key={session._id}
-                className="bg-white p-4 shadow-md rounded-lg flex items-center justify-between"
+                className="bg-white p-4 shadow-md rounded-lg flex flex-col md:flex-row items-center md:items-center justify-between space-y-4 md:space-y-0 md:space-x-4"
               >
-                <div className="flex items-center space-x-4">
-                  {/* Device Image */}
-                  <img
-                    src={process.env.PUBLIC_URL + getDeviceImage(session?.device?.deviceType)}
-                    alt={session?.device?.deviceType}
-                    className="w-12 h-12 object-contain"
-                  />
-            
-                  {/* Device Details */}
+                {/* Device Image */}
+                <img
+                  src={
+                    process.env.PUBLIC_URL +
+                    getDeviceImage(session?.device?.deviceType)
+                  }
+                  alt={session?.device?.deviceType}
+                  className="w-12 h-12 object-contain"
+                />
+
+                {/* Device Details */}
+                <div className="flex-1">
                   <div className="space-y-2">
                     <div className="flex">
                       <Typography className="text-gray-800 font-medium">
@@ -134,35 +137,51 @@ const DeviceManagement: React.FC = () => {
                         {new Date(session?.createdAt).toLocaleString()}
                       </Typography>
                     </div>
-                    {session.currentSession && (
+                  </div>
+
+                  {/* Current Device Chip */}
+                  {session.currentSession && (
+                    <div className="mt-2 md:mt-4 flex md:hidden">
                       <Chip
                         label="Current Device"
                         className="bg-green-100 text-green-700"
                       />
-                    )}
-                  </div>
+                    </div>
+                  )}
                 </div>
-            
+
                 {/* Remove/Logout Button */}
-                <LoadingButton
-                  size="small"
-                  className="bg-red-500 hover:bg-red-600 text-white transition font-thin py-0.5 px-2 text-sm disabled:bg-slate-300"
-                  onClick={() =>
-                    removeSession(session._id, session.currentSession)
-                  }
-                  loading={loadingRemove.includes(session._id)}
-                  loadingPosition="start"
-                  startIcon={
-                    session.currentSession ? (
-                      <LogoutIcon />
-                    ) : (
-                      <RemoveCircleOutlineIcon />
-                    )
-                  }
-                  variant="contained"
-                >
-                  {session.currentSession ? "Log out" : "Remove"}
-                </LoadingButton>
+                <div className="flex flex-col items-center md:items-center space-y-2">
+                  <LoadingButton
+                    size="small"
+                    className={`bg-red-500 hover:bg-red-600 text-white transition font-thin py-0.5 px-2 text-sm disabled:bg-slate-300 ${session.currentSession && "md:my-2"}`}
+                    onClick={() =>
+                      removeSession(session._id, session.currentSession)
+                    }
+                    loading={loadingRemove.includes(session._id)}
+                    loadingPosition="start"
+                    startIcon={
+                      session.currentSession ? (
+                        <LogoutIcon />
+                      ) : (
+                        <RemoveCircleOutlineIcon />
+                      )
+                    }
+                    variant="contained"
+                  >
+                    {session.currentSession ? "Log out" : "Remove"}
+                  </LoadingButton>
+
+                  {/* Current Device Chip for larger screens */}
+                  {session.currentSession && (
+                    <div className="hidden md:flex md:py-2">
+                      <Chip
+                        label="This Device"
+                        className="bg-green-100 text-green-700 px-0"
+                      />
+                    </div>
+                  )}
+                </div>
               </div>
             ))}
           </div>
