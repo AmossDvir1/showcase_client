7ebb55867d9935dc76e04dd2c8f62fd79dc9b7ce
AmossDvir1 <dvir.amoss@gmail.com>
2025-01-04 17:53:36 +0200

    Styles & responsiveness improved in the profile page (relationship status and profile picture)

--- src/components/RelationshipStatusButton.tsx
+++ src/components/RelationshipStatusButton.tsx
@@ -1,17 +1,18 @@
-import { Button } from "@mui/material";
+import { Button, styled, Theme } from "@mui/material";
 import Typography from "./sharedComponents/Typography";
 import React, { useState } from "react";
 import { serverReq } from "../API/utils/axiosConfig";
 import { confirmFriendship } from "../controllers/friendsController/confirmFriendship";
 import Loader from "./sharedComponents/Loader";
+import CustomButton from "./sharedComponents/CustomButton";
+import { Chip } from "./sharedComponents/Chip";
 
 const RequestSent: React.FC = () => {
   return (
-    <div className="flex items-center justify-center xs:p-[0.37rem] lg:p-3 w-fit bg-transparent border-solid border-primary border-[1px] rounded-lg">
-      <Typography className="text-black lg:text-base xs:text-sm text-center">
-        Request Sent
-      </Typography>
-    </div>
+    <Chip
+      className="bg-paper-dark dark:bg-dark-paper-light"
+      label="Request sent"
+    ></Chip>
   );
 };
 interface PendingApprovalProps {
@@ -38,8 +39,8 @@ const PendingApproval: React.FC<PendingApprovalProps> = ({
     }
   };
   return (
-    <div className="flex flex-col items-center justify-center xs:p-[0.37rem] lg:p-3 w-fit bg-transparent border-solid border-primary border-[1px] rounded-lg">
-      <Typography className="text-black lg:text-base xs:text-xs text-center">
+    <div className="flex flex-col items-center justify-center  w-fit bg-transparent  rounded-lg">
+      <Typography className="text-black lg:text-base xs:text-xs text-center pb-2">
         Wants to be your friend
       </Typography>
       <div className="flex flex-row">
@@ -48,8 +49,8 @@ const PendingApproval: React.FC<PendingApprovalProps> = ({
             <Loader></Loader>
           ) : (
             <div className="flex flex-row">
-              <Button onClick={onConfirmClick}>confirm</Button>
-              <Button>delete</Button>
+              <CustomButton size="small" className="md:px-3 xl:px-4 px-0 md:mr-2 mr-1 text-[10px] md:text-[14px] md:h-[inherit] h-8" variant="outlined">delete</CustomButton>
+              <CustomButton size="small" className="md:px-3 xl:px-4 px-0 md:ml-2 ml-1 text-[10px] md:text-[14px] md:h-[inherit] h-8" onClick={onConfirmClick}>confirm</CustomButton>
             </div>
           )}
         </div>
@@ -83,13 +84,17 @@ const RelationshipStatusButton: React.FC<RelationshipStatusButtonProps> = ({
 
   switch (relationship) {
     case "no_relationship":
-      return <Button onClick={onAddUserClick}>+Add friend</Button>;
+      return (
+        <CustomButton className="" size="small" onClick={onAddUserClick}>
+          +Add friend
+        </CustomButton>
+      );
     case "friends":
       return (
-        <div className="flex flex-col items-center justify-center xs:p-[0.37rem] lg:p-3 w-fit bg-transparent border-solid border-primary border-[1px] rounded-lg">
-        <Typography className="text-black lg:text-base xs:text-xs text-center">
-          You are friends
-        </Typography></div>
+        <Chip
+        className="bg-paper-dark dark:bg-dark-paper-light"
+        label="You are friends"
+      ></Chip>
       );
     case "request_sent":
       return <RequestSent></RequestSent>;
